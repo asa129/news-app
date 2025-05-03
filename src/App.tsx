d571e47823fd6f17e1ea009e17c2b4cd817ff5c8
asa129 <asa.jad1290@gmail.com>
2025-05-03 16:28:18 +0900

    あ

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import "./App.css";
 
 const API_KEY = "b3f26b82433b49978da7f07f3d2a5383";
 // const ENDPOINT = `https://newsapi.org/v2/top-headlines?country=us&q=bitcoin&apiKey=${API_KEY}`;
-const ENDPOINT = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`;
+// const ENDPOINT = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`;
 
 function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [inputText, setInputText] = useState("");
+  const [jeminitext, setJeminiText] = useState("");
 
   const onClickButton = async () => {
     const newsapiResponse = await fetch(
@@ -19,7 +19,7 @@ function App() {
 
     setTitle(data.articles[0].title);
     setContent(data.articles[0].description);
-    console.log(data.articles[0]);
+    console.log(data.articles[0].description);
     // 環境変数の読み込み
     const GEMINI_API_KEY: string = "AIzaSyDzHGDwxb8Nakjq_rlp2qvTl3mVwArofmU";
 
@@ -28,13 +28,14 @@ function App() {
 
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-    const prompt = `次の文章を日本語にして、小学生がわかるように変更してください。\n${content}`;
+    const prompt = `次の文章を日本語にして、小学生がわかるように変更してください。\n${data.articles[0].description}`;
     console.log(prompt);
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
-    const text = response.text();
-    console.log(text);
+    const jeminitext = response.text();
+    setJeminiText(jeminitext);
+    console.log(jeminitext);
   };
 
   return (
@@ -45,8 +46,9 @@ function App() {
         onChange={(e) => setInputText(e.target.value)}
       />
       <button onClick={onClickButton}>送信！</button>
-      <p>タイトル：{title}</p>
-      <p>内容：{content}</p>
+      <p>タイトル:{title}</p>
+      <p>内容:{content}</p>
+      <p>Geminiの回答:{jeminitext}</p>
     </>
   );
 }
