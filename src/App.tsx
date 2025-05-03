f7743d27bc1f8f391b63d93f0ef6839eb073594d
asa129 <asa.jad1290@gmail.com>
2025-05-03 15:22:39 +0900

    画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_KEY = "hackMDからとってね";
+  const ENDPOINT = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${API_KEY}`;
+  const onClickButton = () => {
+    fetch(ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    // alert("警告");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" />
+      <button onClick={onClickButton}>送信！</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
